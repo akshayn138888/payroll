a8d20c21a5212e5815cd481ddbf6729a6a249303
EMAP12 <akshayn@sfu.ca>
2016-11-23 20:21:35 +0530

    map updated

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    begin
+      @location = Geocoder.coordinates(current_user.scsc)
+    rescue Exception => e
+      @location = Geocoder.coordinates(current_user.csc)
+    end
   end
 end
