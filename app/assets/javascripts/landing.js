3a646386a36651d1d9ecfac0fdeadf3e32dc739a
EMAP12 <akshayn@sfu.ca>
2016-11-24 19:05:24 +0530

    Landing page css js file are created

---
+++ app/assets/javascripts/landing.js
@@ -0,0 +1,42 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require jquery-ui
+//= require bootstrap-sprockets
+//= require chosen-jquery
+//= require jstz.min
+//= require moment.min
+//= require jquery.cookie.min
+//= require js/easing
+//= require js/wow.min
+//= require js/move-top
+//= require js/modernizr.custom
+//= require js/easyResponsiveTabs
+
+$(document).ready(function(){
+  $(".date").datepicker();
+
+  var tz = jstz.determine();
+  $.cookie('timezone', tz.name());
+  $(".trash_btn_div").hide();
+});
+
+$(document).on({
+    mouseenter: function () {
+    $(this).find(".trash_btn_div").show();
+    },
+    mouseleave: function () {
+      $(this).find(".trash_btn_div").hide();
+    }
+}, ".messages_container");
