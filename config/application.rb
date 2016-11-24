d7868bb9b124c42174adff4455340efe67302038
EMAP12 <akshayn@sfu.ca>
2016-11-24 20:37:38 +0530

    Issue on Asset Pipeline on Heroku 2

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Payroll
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.serve_static_files = true
+    config.assets.compile = true
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
