9092938addb9cae0c41c3816a86b74feb726d409
EMAP12 <akshayn@sfu.ca>
2016-11-24 19:08:42 +0530

    layout landing is assign to rails and render layout landing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,8 @@ class HomeController < ApplicationController
     if current_user
       @location = Geocoder.coordinates(current_user.scsc)
       @location = Geocoder.coordinates(current_user.csc) unless @location.present?
+	else
+	  render :index, layout: "landing"
     end
   end
 end
